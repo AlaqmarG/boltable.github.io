693ba0de7610aec83299282248f61e90136d85e2
Alaqmar Gandhi <alaqmargandhi@gmail.com>
2024-06-26 19:44:04 +0400

    First Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function getMobileWidth() {
+    return 1.4 * document.documentElement.clientHeight;
+}
+
+function updateLayout() {
+    const text = document.getElementById("text");
+    const image = document.getElementById("image");
+
+    if (document.documentElement.clientWidth < getMobileWidth()) {
+        image.style.right = null;
+        image.style.left = "50%";
+        image.style.transform = "translate(-50%)";
+
+        text.style.left = "50%";
+    } else {
+        image.style.left = null;
+        image.style.transform = null;
+        image.style.right = "-300px"
+
+        text.style.left = "25%";
+    }
+}
+
+window.addEventListener("resize", updateLayout)
